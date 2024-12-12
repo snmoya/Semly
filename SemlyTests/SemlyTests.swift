a4de0030b3c58d7da2145998b1668c3a2a511220
Sebastian <moya.sebastian@hotmail.com>
2024-12-12 13:35:33 +1000

    Initial Commit

---
+++ SemlyTests/SemlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SemlyTests.swift
+//  SemlyTests
+//
+//  Created by Sebastian Moya on 12/12/2024.
+//
+
+import Testing
+@testable import Semly
+
+struct SemlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

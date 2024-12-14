b47839d1abcc4e3e6071a769c29691e32644f1ac
Sebastian <moya.sebastian@hotmail.com>
2024-12-14 12:26:16 +1000

    Added Colors Palette to use throut the project

---
+++ Semly/Views/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  Semly
+//
+//  Created by Sebastian Moya on 14/12/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primaryOrange = Color("PrimaryOrange") // From Assets
+    static let primaryGray = Color("PrimaryGray") // From Assets
+    static let deepRed = Color(red: 211/255, green: 47/255, blue: 47/255)
+    static let oliveGreen = Color(red: 139/255, green: 195/255, blue: 74/255)
+    static let warmYellow = Color(red: 255/255, green: 214/255, blue: 0/255)
+    static let purpleAccent = Color(red: 126/255, green: 87/255, blue: 194/255)
+    static let coralPink = Color(red: 255/255, green: 112/255, blue: 67/255)
+    static let softGray = Color(red: 245/255, green: 245/255, blue: 245/255)
+    static let charcoal = Color(red: 48/255, green: 56/255, blue: 65/255)
+}
